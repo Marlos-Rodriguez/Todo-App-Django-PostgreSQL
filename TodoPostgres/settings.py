5ae0314f448874c0b4a22e80c3247afe6b804cb0
Marlos <Marlos2811@hotmail.com>
2020-09-13 12:21:17 -0300

    Password hash

--- TodoPostgres/settings.py
+++ TodoPostgres/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'TodoDB',
         'USER': 'postgres',
-        'PASSWORD': 'Elmejordiademivida2009',
+        'PASSWORD': '******',
         'HOST': 'localhost',
         'PORT': '5432'
     }
